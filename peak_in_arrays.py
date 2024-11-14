8121de4bc7499ce23998d7bbf14e4a3fa35febc6
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2024-11-14 15:09:43 +0200

    created the function

--- peak_in_arrays.py
+++ peak_in_arrays.py
@@ -2,3 +2,12 @@
 array = [2, 8, 3, 7, 9, 2, 10, 11, 15, 1]
 def find_peaks(array):
     length = len(array) - 2
+    indices = [] # list of peaks 
+    for i in range(length): 
+        if array[i] < array[i + 1] > array[i + 2]: 
+            indices.append(array[i +1])
+    return indices 
+print(find_peaks(array))
+
+
+
